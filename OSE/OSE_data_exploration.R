54a7b526813092c32e670c3b445acb7546e47d3a
Rachel Bash <reb64@duke.edu>
2019-11-21 16:58:41 -0500

    started on organizing OSE shp file

--- OSE/OSE_data_exploration.R
+++ OSE/OSE_data_exploration.R
@@ -1,3 +1,14 @@
+library(sf)
+library(tidyverse)
+
 getwd()
-OSE <- read_sf("./OSE_Points_of_Diversion/OSE_Points_of_Diversion.shp")
+OSE <- read_sf("../OSE_Points_of_Diversion/OSE_Points_of_Diversion.shp")
 names(OSE)
+names(NGWMN_site_skinny)
+
+OSE <- OSE %>% select(pod_nbr, county, elevation, depth_well, depth_wate, utm_zone, easting,
+                      northing, datum, aquifer, city, state, zip, geometry, 
+                      lat_deg, lat_min, lat_sec, lon_deg, lon_min, lon_sec,
+                      start_date, log_file_d, sys_date)
+
+
