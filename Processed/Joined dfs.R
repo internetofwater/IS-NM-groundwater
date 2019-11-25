ee055b719b2ef3fc13d878eef2ec2783cf456c70
Rachel Bash <reb64@duke.edu>
2019-11-25 10:22:27 -0500

    push new data

--- Processed/Joined dfs.R
+++ Processed/Joined dfs.R
@@ -27,8 +27,9 @@ names(USGS.gwls) <- c("AgencyCd", "SiteNo", "site_tp_cd.USGS", "Date", "Time",
 
 gwl.joined <- rbind.fill(NGWMN.gwls, USGS.gwls)
 gwl.joined.skinny <- gwl.joined %>%
-  select(AgencyCd, SiteNo, Date, Time, Depth.to.Water.Below.Land.Surface.in.ft., Accuracy.Value,
-         Water.level.in.feet.relative.to.NAVD88, Observation.Method, Comment)
+  select(AgencyCd, SiteNo, Date, Time, Depth.to.Water.Below.Land.Surface.in.ft.)
+
+write.csv(gwl.joined, file = "./Processed/gwl.joined.csv")
 
 write.csv(gwl.joined.skinny, file="./Processed/gwl.joined.skinny.csv")
 
@@ -40,8 +41,7 @@ USGS.sites <- read.csv("./USGS/USGS.site.csv")
 USGS.sites <- USGS.sites %>% select(-X)
 NGWMN.sites <- read.csv("./NGWMN/NGWMN.site.skinny.csv") 
 NGWMN.sites <- NGWMN.sites %>% select(-X)
-NMBGMR.sites <- read.csv("./NMBGMR/NMBGMR.site.csv")
-NMBGMR.sites <- NMBGMR.sites %>% select(-X)
+#add in correct meta data for NMBGMR sites
 
 
 str(USGS.sites)
@@ -65,19 +65,37 @@ names(USGS.sites) <-
     "project_no.USGS", "AgencyNm")
 
 sites.joined <- rbind.fill(NGWMN.sites, USGS.sites)
+sites.joined$OSEWellID <- ""
 
 sites.joined <- rbind.fill(sites.joined, NMBGMR.sites)
 names(sites.joined)
 
 sites.joined.skinny <- sites.joined %>%
   select(AgencyCd, SiteNo, AgencyNm, SiteName, OSEWellID, DecLatVa, DecLongVa, 
-         HorzDatum, AltVa, AltDatumCd, CountyNm, SiteType, WellDepth)
+         HorzDatum, AltVa, AltDatumCd, CountyNm, SiteType, WellDepth, LocalAquiferName)
 
+
+write.csv(sites.joined, file = "./Processed/sites.joined.csv")
 write.csv(sites.joined.skinny, file="./Processed/sites.joined.skinny.csv")
 
+
+
+#Join of both gwl and meta data....probably too big.
 gwl.sites.joined <- left_join(gwl.joined.skinny, sites.joined.skinny, 
                               by = c("SiteNo", "AgencyCd"))
 
 write.csv(gwl.sites.joined, file = "./Processed/gwl.sites.joined.csv")
 
 
+#--------create static gwl summarized df-----------#
+gwl.joined.skinny$Date <- as.Date(gwl.joined.skinny$Date)
+gwl.joined.skinny.static <- gwl.joined.skinny %>%
+  dplyr::group_by(AgencyCd, SiteNo) %>%
+  dplyr::summarise(
+    firstMeas = min(Date),
+    lastMeas = max(Date), 
+    Count = length(SiteNo)
+  )
+
+sites.summary.static <- left_join(sites.joined.skinny, gwl.joined.skinny.static, by = "SiteNo")
+write.csv(sites.summary.static, file = "./Processed/sites.summary.static.csv")
